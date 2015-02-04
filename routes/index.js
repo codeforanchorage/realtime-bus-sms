39bd1e5a7b04ca9e81cab51f9ca35b4499a3f189
Nigel Kibodeaux <nigelmail@gmail.com>
2015-02-04 09:29:45 -0900

    converted to use Express
    disclaimer: the code is in a messy state right now

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+var stop_number_lookup = require('../lib/stop_number_lookup');
+var debug = require('debug')('routes/index.js');
+var getStopData = require('../lib/index');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+/* POST home page */
+router.post('/', function(req, res, next) {
+  var stopId = req.body.Body;
+  var bustrackerId = stop_number_lookup[stopId];
+
+  if (!bustrackerId) {
+      debug('Bad input');
+      debug(stopId);
+      res.send('Invalid stop number');
+  }
+  else {
+      getStopData(bustrackerId, function(err, data) {
+          debug('Good input');
+
+          res.set('Content-Type', 'text/plain');
+          res.send(data);
+      })
+  }
+});
+
+module.exports = router;
