27a962bf9ca5490e144e447b1e0a3ecc342071ad
Jessica Austin <jessica@aus10.org>
2015-02-21 15:43:49 -0900

    restructure to add additional input

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var stop_number_lookup = require('../lib/stop_number_lookup');
 var debug = require('debug')('routes/index.js');
-var getStopData = require('../lib/index');
+var lib = require('../lib/index');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,21 +11,37 @@ router.get('/', function(req, res, next) {
 
 /* POST home page */
 router.post('/', function(req, res, next) {
-  var stopId = req.body.Body;
-  var bustrackerId = stop_number_lookup[stopId];
+  var stopId = req.body.stopNumber;
+  var intersection = req.body.intersection;
 
-  if (!bustrackerId) {
-      debug('Bad input');
-      debug(stopId);
-      res.send('Invalid stop number');
-  }
-  else {
-      getStopData(bustrackerId, function(err, data) {
-          debug('Good input');
+  console.dir(lib)
 
-          res.set('Content-Type', 'text/plain');
-          res.send(data);
-      })
+  if (stopId) {
+    var bustrackerId = stop_number_lookup[stopId];
+
+    if (!bustrackerId) {
+        debug('Bad input');
+        debug(stopId);
+        res.send('Invalid stop number');
+    }
+    else {
+        lib.getStopData(bustrackerId, function(err, data) {
+            debug('Good input');
+
+            res.set('Content-Type', 'text/plain');
+            res.send(data);
+        })
+    }
+  } 
+  else if (intersection) {
+    lib.getStopFromAddress(intersection, function(err, data) {
+        debug('Good input');
+
+        res.set('Content-Type', 'text/plain');
+        res.send(data);
+    })
+  } else {
+    res.send('No input');
   }
 });
 
