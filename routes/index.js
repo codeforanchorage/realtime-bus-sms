83451af88105f0cc39b9a2a4969d07c876683c5f
Mark Meyer <mark@photo-mark.com>
2016-10-20 14:56:13 -0800

    Reintegrating the logging function

--- routes/index.js
+++ routes/index.js
@@ -15,11 +15,13 @@ var fs = require('fs');
 var twilioClient = require('twilio')(config.TWILIO_ACCOUNT_SID, config.TWILIO_AUTH_TOKEN);
 
 
+
 // Log format:
 // message is whatever the user sends
 // stop is the stop that we've parsed from the message
 // data is the current datetime
 // if it's sent from twiliio, we store a human-readable hash of the #
+
 function logRequest(entry) {
     entry.date = new Date();
     db_private('requests').push(entry);
@@ -31,21 +33,18 @@ function logRequest(entry) {
     db('requests').push(entry2);
 }
 
-function logIt(req, res, next){
-    var entry = {
-        input: altInput || req.body.Body,
-        stop: data.route,
+function startLogging(req, res, next){
+     res.locals.logEntry = {
+        input: req.body.Body,
+        stop: "",
         phone: req.body.From,
         ip: req.connection.remoteAddress,
-        geocodedAddress: geocodedAddress,
-        totalTime: req.start ? (Date.now() - req.start) : "",  // Should really go on res.on('finish'), but do we want to only log on successful sends?
-        muniTime: muniTime || ""
+        geocodedAddress: "",
+        totalTime: "",  // Should really go on res.on('finish'), but do we want to only log on successful sends?
+        muniTime:  "",
+        geocodeTime: ""
     };
-    logRequest(entry)
-}
-
-function startLogTimer(req, res, next){
-    req.start = Date.now();
+    res.locals.startTime = Date.now();
     next();
 }
 
@@ -62,25 +61,37 @@ function parseBody(req, res, next){
     }
     // the message is only digits or # + digits or "stop" + (#) + digits -- assume it's a stop number
     var stopMessage = message.toLowerCase().replace(/ /g,'').replace("stop",'').replace("#",'');
+    var log = res.locals.logEntry;
     if (/^\d+$/.test(stopMessage)) {
         lib.getStopFromStopNumber(parseInt(stopMessage))
-        .then((data) => res.render('stop-list-partial', {route:data}))
+        .then((routeObject) => {
+            log.muniTime = routeObject.asyncTime;
+            log.stop = routeObject.data.stopId || "";
+            log.totalTime = Date.now() - res.locals.startTime;
+            logRequest(log);
+            res.render('stop-list-partial', {route:routeObject.data})
+        })
         .catch((err) => res.render('message', {message: err}));
     } else {
-         lib.getStopsFromAddress(req.body.Body)
-         .then((data) => res.render('route-list-partial', {routes:data}))
-         .catch((err) => res.render('message', {message: err})); 
+        lib.getStopsFromAddress(req.body.Body)
+        .then((routeObject) => {        
+            log.geocodedAddress = routeObject.data.geocodedAddress || "";
+            log.geocodeTime = routeObject.asyncTime;
+            log.totalTime = Date.now() - res.locals.startTime;
+            logRequest(log);
+            res.render('route-list-partial', {routes:routeObject.data.stops})})
+        .catch((err) => res.render('message', {message: err})); 
     } 
+
     return
 }
-
-
-router.use(startLogTimer);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index');
 });
+
+router.use(startLogging); //after get('/') means homepage requests don't get logged.
 
 // Twilio hits this endpoint. The user's text message is
 // in the POST body.
@@ -110,22 +121,22 @@ router.post('/ajax', function(req, res, next) {
 // a browser with location service enabled can hit this
 router.get('/byLatLon', function(req, res, next) {
     res.locals.returnHTML = 1;
+
     if (lib.serviceExceptions()) {
         res.locals.error = {message:'No Service - Holiday'};
         res.render('message')
         return;
     }
     if (!req.query.lat || !req.query.lon){
-         res.render('message', {message: {message: "Can't determine your location"}});
-         return;
+        req.logEntry.geocodedAddress = "not found";
+        res.render('message', {message: {message: "Can't determine your location"}});
+        return;
      }
      var data = lib.findNearestStops(req.query.lat, req.query.lon);
-     console.log("data", data);
      if (!data || data.length == 0){
          res.render('message', {message: {message: "No routes found near you"}});
          return;
      }
-
      res.render('route-list-partial', {routes:  data });
 });
 
