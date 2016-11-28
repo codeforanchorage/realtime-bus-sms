3247ff77961d1d7e5b735eae4dc40bfd2e1287a0
Mark Meyer <mark@photo-mark.com>
2016-11-27 15:15:23 -0900

    first attempt at hooking up Google Analytics

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
+var logs = require('./lib/logTransport');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -19,27 +20,42 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-logger.token('stop', function(req, res){
-    if(res.locals.routes){
-        return res.locals.routes.data.stopId || ""
-    }
-    return "";
+
+app.use(logs())
+/*
+logger.token('stop', (req, res) => {
+    return res.locals.routes
+    ?  res.locals.routes.data.stopId || undefined
+    : undefined;
 })
-logger.token('geoCodedAddress', function(req, res){
-    if(res.locals.routes){
-        return res.locals.routes.data.geocodedAddress || ""
-    }
-    return "";
+logger.token('geoCodedAddress', (req, res) => {
+    return res.locals.routes 
+        ? res.locals.routes.data.geocodedAddress || undefined
+        : undefined;
 })
+logger.token('muniTime', (req, res) => {
+    return res.locals.routes 
+        ? res.locals.routes.muniTime || undefined
+        : undefined;
+})
+logger.token('geocodeTime', (req, res) => {
+    return res.locals.routes 
+        ? res.locals.routes.geocodeTime || ""
+        : "";
+})
+
 //app.use(logger('common'));
-app.use(logger(':remote-addr - [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :stop :geoCodedAddress ', 
-    {
-        skip: function(req, res) {
-                return res.statusCode < 400 && ( req.url.startsWith('/css') || req.url.startsWith('/javascripts') || req.url.startsWith('/img'))
-        }
+app.use(logger(':remote-addr - [:date[iso]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time :muniTime :geocodeTime :stop :geoCodedAddress', {
+    skip: function(req, res) {
+            return res.statusCode < 400 && ( 
+                req.url.startsWith('/css') 
+                || req.url.startsWith('/javascripts') 
+                || req.url.startsWith('/img')
+                )},
+    stream: logTransport.stream
     }));
 
-
+*/
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
