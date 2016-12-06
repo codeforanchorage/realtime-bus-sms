9b6356659f032334e859cb50a603dbd5b0358510
Mark Meyer <mark@photo-mark.com>
2016-12-05 15:07:26 -0900

    decoupling logging from the rest of the application

--- app.js
+++ app.js
@@ -22,7 +22,49 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 
-app.use(logs.log)
+app.use(logs.initialize((req, res) => {
+    var routes = res.locals.routes
+    return {
+        input:           req.body.Body,
+        phone:           req.body.From,
+        muniTime:        routes ? routes.muniTime: undefined,
+        geocodeTime:     routes ? routes.geocodeTime: undefined,
+        stopId:          routes ? routes.data.stopId: undefined,
+        geocodedAddress: routes ? routes.data.geocodedAddress: undefined,
+        action:          res.locals.action,
+    }
+}));
+
+/*  
+    Setup Google Analytics
+    The convention used here is:
+    category: 'sms | 'web'
+    action: actions are set by the router depending on what the user was looking for
+           currently: '[Failed?]Stop Lookup' '[Failed?]Address Lookup', 'Empty Input', 'About', 'Feedback'
+    label: the actual search: the stop number, geocoded address, or the raw input if lookup failed
+
+*/
+logs.initGoogleAnalytics((logFields) => {
+    // UUID helps seperate new and returning users on Twilio
+    var uuid;
+    var category = logFields.phone ? "sms" : "web";
+    if (category == "sms"){
+        var ns = "deebee62-076c-47ef-ad02-2509e2d4f839" // this random namespace is hashed (using SHA-1) with phone number to create UUID
+        uuid = new UUID(5, ns, meta.phone).format()
+    }
+    return {
+        trackingCode: config.GOOGLE_ANALYTICS_ID,
+        category:     category,
+        action:       logFields.action,
+        label:        logFields.stopId 
+                      || logFields.geocodedAddress 
+                      || (logFields.input ? logFields.input.trim().toLowerCase(): ""),
+        value:        logFields.responseTime,
+        uuid:         uuid,
+        timings:      [{name: "muniTime", time: logFields.muniTime }]
+
+    }
+})
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
