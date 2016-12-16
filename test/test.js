cb0ab903bcef81a94f663e90f69fe068c019bc85
Mark Meyer <mark@photo-mark.com>
2016-12-16 11:31:43 -0900

    Changed the test address in test_browserAddressEntry & test_smsAddressEntry to '5th Avenue and G street'. The previous address was '5th Avenue' for which Google's geocoder intermittently returns zero results causing the tests to fails

--- test/test.js
+++ test/test.js
@@ -134,7 +134,7 @@ exports.group = {
 
 // Test an address entry
     test_browserAddressEntry: function (test) {
-        var address = "5th Avenue";
+        var address = "5th Avenue and G street";
         api.post(test, '/ajax', {
             data: {Body: address}
         }, function (res) {
@@ -142,7 +142,7 @@ exports.group = {
         });
     },
     test_smsAddressEntry: function (test) {
-        var address = "5th Avenue";
+        var address = "5th Avenue and G street";
         api.post(test, '/', {
             data: {Body: address}
         }, function (res) {
